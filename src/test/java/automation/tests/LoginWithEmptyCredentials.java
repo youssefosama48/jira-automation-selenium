45d7e4ad2b19d9d2f2b91bf923b20e0d6a9f1a4e
youssefosama48 <youssefosama4821@gmail.com>
2026-03-15 16:37:23 +0200

    test(TE-137): add LoginWithEmptyCredentials for TE-T185

---
+++ src/test/java/automation/tests/LoginWithEmptyCredentials.java
@@ -0,0 +1,64 @@
+package automation.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginWithEmptyCredentials {
+    
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private static final String APP_URL = "https://demo.guru99.com/V4/";
+    
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
+        ChromeOptions opts = new ChromeOptions();
+        if (headless) {
+            opts.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+        }
+        driver = new ChromeDriver(opts);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    
+    @Test
+    public void testLoginWithEmptyCredentials() {
+        driver.get(APP_URL);
+        
+        WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("uid")));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.name("btnLogin"));
+        
+        assertTrue(usernameField.isDisplayed());
+        assertTrue(passwordField.isDisplayed());
+        
+        assertEquals("", usernameField.getAttribute("value"));
+        assertEquals("", passwordField.getAttribute("value"));
+        
+        loginButton.click();
+        
+        wait.until(ExpectedConditions.alertIsPresent());
+        String alertText = driver.switchTo().alert().getText();
+        assertTrue(alertText.contains("User or Password is not valid"));
+        driver.switchTo().alert().accept();
+    }
+    
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
