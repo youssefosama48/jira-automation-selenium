38c32e06497dcd470ccf9e4963b253d7cdb0fdf4
youssefosama48 <youssefosama4821@gmail.com>
2026-03-15 16:47:55 +0200

    test(TE-137): add LoginWithInvalidUsernameFormat for TE-T211

--- src/test/java/automation/tests/LoginWithInvalidUsernameFormat.java
+++ src/test/java/automation/tests/LoginWithInvalidUsernameFormat.java
@@ -10,47 +10,49 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.time.Duration;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginWithInvalidUsernameFormat {
-    
+
     private WebDriver driver;
     private WebDriverWait wait;
-    private static final String APP_URL = "https://demo.guru99.com/V4/";
-    private static final String INVALID_USERNAME = "user@";
-    private static final String PASSWORD = "testpass123";
-    
+    private final String APP_URL = "https://demo.guru99.com/V4/";
+
     @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         boolean headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
         ChromeOptions opts = new ChromeOptions();
-        if (headless) opts.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+        if (headless) {
+            opts.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+        }
         driver = new ChromeDriver(opts);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get(APP_URL);
+        driver.manage().window().maximize();
     }
-    
+
     @Test
     public void testLoginWithInvalidUsernameFormat() {
-        WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("uid")));
+        driver.get(APP_URL);
+        
+        WebElement usernameField = wait.until(ExpectedConditions.elementToBeClickable(By.id("uid")));
+        assertTrue(usernameField.isDisplayed(), "Login page should load successfully");
+
+        usernameField.sendKeys("@#$%");
         WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("Test123!");
+
         WebElement loginButton = driver.findElement(By.name("btnLogin"));
-        
-        assertTrue(usernameField.isDisplayed(), "Login form should be displayed");
-        
-        usernameField.sendKeys(INVALID_USERNAME);
-        passwordField.sendKeys(PASSWORD);
         loginButton.click();
-        
-        wait.until(ExpectedConditions.alertIsPresent());
-        String alertText = driver.switchTo().alert().getText();
+
+        WebElement errorAlert = wait.until(ExpectedConditions.alertIsPresent()).switchTo().alert();
+        String alertText = errorAlert.getText();
         assertTrue(alertText.contains("User or Password is not valid"), "Username format error should be displayed");
-        driver.switchTo().alert().accept();
+        errorAlert.accept();
     }
-    
+
     @AfterEach
     public void tearDown() {
         if (driver != null) {
