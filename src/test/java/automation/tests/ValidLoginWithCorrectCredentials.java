38750dc002d38cef16ac9f899883f8366ab161e8
youssefosama48 <youssefosama4821@gmail.com>
2026-03-15 16:41:14 +0200

    test(TE-137): add ValidLoginWithCorrectCredentials for TE-T193

--- src/test/java/automation/tests/ValidLoginWithCorrectCredentials.java
+++ src/test/java/automation/tests/ValidLoginWithCorrectCredentials.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,15 +14,14 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ValidLoginWithCorrectCredentials {
-    
     private WebDriver driver;
     private WebDriverWait wait;
     private static final String APP_URL = "https://demo.guru99.com/V4/";
     private static final String USERNAME = "testuser";
-    private static final String PASSWORD = "Test123!";
-    
+    private static final String PASSWORD = "Test123";
+
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         WebDriverManager.chromedriver().setup();
         boolean headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
         ChromeOptions opts = new ChromeOptions();
@@ -32,27 +30,26 @@ public class ValidLoginWithCorrectCredentials {
         }
         driver = new ChromeDriver(opts);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().window().maximize();
     }
-    
+
     @Test
-    public void testValidLoginWithCorrectCredentials() {
+    void testValidLoginWithCorrectCredentials() {
         driver.get(APP_URL);
         
-        WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("uid")));
-        assertTrue(usernameField.isDisplayed(), "Login form should be displayed");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("uid")));
+        assertTrue(driver.findElement(By.id("uid")).isDisplayed());
         
-        usernameField.sendKeys(USERNAME);
+        driver.findElement(By.id("uid")).sendKeys(USERNAME);
         driver.findElement(By.id("password")).sendKeys(PASSWORD);
         
         driver.findElement(By.name("btnLogin")).click();
         
         wait.until(ExpectedConditions.urlContains("Managerhomepage"));
-        assertTrue(driver.getCurrentUrl().contains("Managerhomepage"), "User should be redirected to dashboard");
+        assertTrue(driver.getCurrentUrl().contains("Managerhomepage"));
     }
-    
+
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         if (driver != null) {
             driver.quit();
         }
